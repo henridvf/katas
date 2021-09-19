5928d080eaa05e7a2a50f4d3a21c59e161a9384f
Henri <henridvf@gmail.com>
2021-09-19 22:50:59 +0100

    Fixed the final return message; cleaned up code

--- duration_format.py
+++ duration_format.py
@@ -1,5 +1,4 @@
 def format_duration(seconds):
-
     if seconds == 0:
         return 'now'
 
@@ -8,17 +7,13 @@ def format_duration(seconds):
     years = seconds // (365 * 24 * 60 * 60)
     res = seconds - (years * 365 * 24 * 60 * 60)
 
-    if res > 86400:
+    if res >= 86400:
         days = res // (24 * 60 * 60)
         res -= days * 24 * 60 * 60
 
-    if res > 3600:
+    if res >= 3600:
         hours = res // (60 * 60)
         res -= hours * 60 * 60
-
-    if res > 0:
-        mins = res // 60
-        res -= mins * 60
 
     if res > 59:
         mins += res // 60
@@ -27,35 +22,33 @@ def format_duration(seconds):
         secs = res
 
     # time formatting
-    msg_years, msg_days, msg_hours, msg_mins, msg_secs = '','','','',''
-    has_years, has_days, has_hours, has_mins, has_secs = False, False, False, False, False
 
+    lst = []
     if years >= 1:
         msg_years = f"{years} year{'s' if years > 1 else ''}"
-        has_years = True;
+        lst.append(msg_years)
 
     if days >= 1:
         msg_days = f"{days} day{'s' if days > 1 else ''}"
-        has_days = True
+        lst.append(msg_days)
 
     if hours >= 1:
         msg_hours = f"{hours} hour{'s' if days > 1 else ''}"
-        has_hours = True
+        lst.append(msg_hours)
 
     if mins >= 1:
         msg_mins = f"{mins} minute{'s' if mins > 1 else ''}"
-        has_mins = True
+        lst.append(msg_mins)
 
     if secs >= 1:
         msg_secs = f"{secs} second{'s' if secs > 1 else ''}"
-        has_secs = True
-
+        lst.append(msg_secs)
 
     # display formatting
+    msg = ''.join([(', ' if x != lst[-1] else ' and ') + str(x) for x in lst])
 
-    msg = f"{msg_years}, {msg_days}, {msg_hours}, {msg_mins}, {msg_secs}"
-
-    return msg, years, days, hours, mins, secs
+    return msg[2:]
 
 
-print(format_duration(90062))
+# print(format_duration(3660))
+print(format_duration(23423456))
